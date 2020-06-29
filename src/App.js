3e7885ba08a156438d5666c76732135d83cee6df
Mahdie <mahdiechappi@gmail.com>
2020-06-29 11:58:59 -0700

    Final -_-

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [latest,setLatest] = useState('');
   useEffect(()=>{
     axios
-    .get('https://api.themoviedb.org/3/movie/popular?api_key=178a04566db87fea763461f13e269845')
+    .get('https://api.themoviedb.org/3/movie/419704?api_key=178a04566db87fea763461f13e269845')
   .then(res => {
     setLatest(res.data);
   } )
@@ -23,34 +23,12 @@ function App() {
   <Card bg='light' text='dark' className='text-center' style={{margin:'20px'}}>
     <Card.Img variant="center" src="http://image.tmdb.org/t/p/w342/\/xBHvZcjRiWyobQ9kxBhO6B2dtRI.jpg" />
     <Card.Body>
-      <Card.Title>Ad Astra</Card.Title>
+      <Card.Title>{latest.original_title}</Card.Title>
       <Card.Text>
-       popularity: 300.628
+       popularity: {latest.popularity}
       </Card.Text>
     </Card.Body>
   </Card>
-
-  <Card bg='light' text='dark' className='text-center' style={{margin:'20px'}}>
-    <Card.Img variant="center" src="http://image.tmdb.org/t/p/w342/\/tI8ocADh22GtQFV28vGHaBZVb0U.jpg" />
-    <Card.Body>
-      <Card.Title>Artemis Fowl</Card.Title>
-      <Card.Text>
-       popularity: 142.749
-      </Card.Text>
-    </Card.Body>
-  </Card>
-
-  <Card bg='light' text='dark' className='text-center' style={{margin:'20px'}}>
-    <Card.Img variant="center" src="http://image.tmdb.org/t/p/w342/\/db32LaOibwEliAmSL2jjDF6oDdj.jpg" />
-    <Card.Body>
-      <Card.Title>Star Wars: The Rise of Skywalker</Card.Title>
-      <Card.Text>
-       popularity: 132.206
-      </Card.Text>
-    </Card.Body>
-  </Card>
-
-  
 </CardDeck>
     </div>
   );
