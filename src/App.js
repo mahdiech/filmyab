6fc6501ae891bdbcba773b9b518902bb6e1460b7
Mahdie <mahdiechappi@gmail.com>
2020-06-29 11:24:04 -0700

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React , {useEffect} from 'react';
+import React , {useEffect,useState} from 'react';
 import Card from 'react-bootstrap/Card';
 import CardDeck from 'react-bootstrap/CardDeck';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 
 function App() {
+  const[latest,setLatest] = useState('');
 useEffect(()=>{
   axios
-  .get('https://api.themoviedb.org/3/discover/movie?api_key=178a04566db87fea763461f13e269845&primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22')
+  .get('https://api.themoviedb.org/3/movie/419704?api_key=178a04566db87fea763461f13e269845')
 .then(res => {
-  console.log(res.data);
+  setLatest(res.data);
 } )
 .catch(err => {
   console.log(err);
@@ -17,32 +18,32 @@ useEffect(()=>{
 
 },[]);
   return (
-    <div className="App">
-    <CardDeck>
+    <div className="App" >
+    <CardDeck >
   <Card bg='light' text='dark' className='text-center' style={{margin:'20px'}}>
-    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Img variant="center" src="http://image.tmdb.org/t/p/w342/\/xBHvZcjRiWyobQ9kxBhO6B2dtRI.jpg" />
     <Card.Body>
-      <Card.Title>movie name</Card.Title>
+      <Card.Title>{latest.original_title}</Card.Title>
       <Card.Text>
-        about
+       popularity: {latest.popularity}
       </Card.Text>
     </Card.Body>
   </Card>
   <Card bg='light' text='dark' className='text-center' style={{margin:'20px'}}>
-    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Img variant="top" src={latest.poster_path} />
     <Card.Body>
-      <Card.Title>movie name</Card.Title>
+      <Card.Title>Title</Card.Title>
       <Card.Text>
-        about
+      popularity
       </Card.Text>
     </Card.Body>
   </Card>
   <Card bg='light' text='dark' className='text-center' style={{margin:'20px'}}>
-    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Img variant="top" src={latest.poster_path} />
     <Card.Body>
-      <Card.Title>movie name</Card.Title>
+      <Card.Title>Title</Card.Title>
       <Card.Text>
-        about
+      popularity
       </Card.Text>
     </Card.Body>
   </Card>
