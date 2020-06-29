d8518dbafa77ebb2d6e37fa29317ff54e2f7dbad
Mahdie <mahdiechappi@gmail.com>
2020-06-29 08:05:36 -0700

    First Commit - start project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hi db movie :))
+    </div>
+  );
+}
+
+export default App;
